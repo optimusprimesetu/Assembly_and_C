c1aa5b5f19514d8dae17207e9af73e120180e471
optimusprimesetu <109286391+optimusprimesetu@users.noreply.github.com>
2026-03-24 12:00:20 +0000

    Makefile in part2

---
+++ PRACTICAL_08/practical_08_part2/src/introduction/lesson_12/main.s
@@ -0,0 +1,52 @@
+	.file	"main.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Result : %d\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$4, %edx
+	movl	$3, %ecx
+	call	add
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.globl	add
+	.def	add;	.scl	2;	.type	32;	.endef
+	.seh_proc	add
+add:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	movl	16(%rbp), %edx
+	movl	24(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (Rev6, Built by MSYS2 project) 13.2.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
